f63d752e6f8d4653a86328ee1364322ddeeee028
LiBossOK <714446076@qq.com>
2022-12-12 21:23:48 +0800

    handlerInterceptor

--- demo/src/main/java/com/example/demo/config/MyMvcConfig.java
+++ demo/src/main/java/com/example/demo/config/MyMvcConfig.java
@@ -1,12 +1,15 @@
 package com.example.demo.config;
 
+import com.example.demo.interceptor.LoginInterceptor;
 import com.example.demo.messageConvert.CustomMessageConvert;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 import org.springframework.web.accept.ParameterContentNegotiationStrategy;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
@@ -22,15 +25,25 @@ public class MyMvcConfig{
     public WebMvcConfigurer webMvcConfigurer(){
             return new WebMvcConfigurer(){
                 @Override
+                public void addInterceptors(InterceptorRegistry registry) {
+                    registry.addInterceptor(new LoginInterceptor())
+                            .addPathPatterns("/**")//拦截所有请求
+                            .excludePathPatterns("/login","/static/**");//放行登录页面的请求，静态资源的请求
+                }
+
+                @Override
                 public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-                    //自定义参数协商策略(内容协商来决定我们可以处理什么类型的请求）
+
                     //这里只配置了参数的协商策略，所以其他的就不会被匹配到，要用请求的协商策略，需要将其也配置上
                     Map<String, MediaType> mediaTypes = new HashMap<>();
                     mediaTypes.put("json",MediaType.APPLICATION_JSON);
                     mediaTypes.put("xml",MediaType.APPLICATION_XML);
                     mediaTypes.put("lsl",MediaType.parseMediaType("applicaton/x-lsl"));
+                    //自定义参数协商策略(内容协商来决定我们可以处理什么类型的请求）
                     ParameterContentNegotiationStrategy parameterStrategy= new ParameterContentNegotiationStrategy(mediaTypes);
-                    configurer.strategies(Arrays.asList(parameterStrategy));
+                    //基于请求头的消息协商策略
+                    HeaderContentNegotiationStrategy headerStrategy = new HeaderContentNegotiationStrategy();
+                    configurer.strategies(Arrays.asList(parameterStrategy,headerStrategy));
                 }
 
                 public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
---
+++ demo/src/main/java/com/example/demo/interceptor/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.example.demo.interceptor;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+/**
+ * 将拦截的地址配置好，
+ * 将其放在容器中
+ * */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        if(session.getAttribute("users")!=null){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
